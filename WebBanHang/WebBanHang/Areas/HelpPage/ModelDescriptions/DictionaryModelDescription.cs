29800d9bde23ca0f963c4270e926f225a0c889ee
nguyenbac <nvbac@tma.com.vn>
2016-08-28 16:19:08 +0700

    this is the first commit from bacnguyen

---
+++ WebBanHang/WebBanHang/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebBanHang.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebBanHang/WebBanHang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebBanHang.WebApiApplication" Language="C#" %>
