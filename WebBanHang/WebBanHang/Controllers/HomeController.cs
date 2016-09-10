602f1795393c71f95b0f558294f38621f5de61fd
nguyenbac <nvbac@tma.com.vn>
2016-09-10 13:00:15 +0700

    nguyen

--- WebBanHang/WebBanHang/Controllers/HomeController.cs
+++ WebBanHang/WebBanHang/Controllers/HomeController.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebBanHang.Models;
 
 namespace WebBanHang.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
             return View();
         }
     }
--- WebBanHang/WebBanHang/Controllers/ProductController.cs
+++ WebBanHang/WebBanHang/Controllers/ProductController.cs
@@ -1,19 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using WebBanHang.Models;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Web.Mvc;
 
 namespace WebBanHang.Controllers
 {
-    public class ProductController : ApiController
+    public class ProductController : Controller
     {
-        // GET: api/products
-        //public Enumberable<Product> Get()
-        //{
-        //    return null;
-        //}
+        SalesContext sales = new SalesContext();
 
+        // GET: Product/ ByCat / id
+        public ActionResult ByCat(int id)
+        {
+            List<Product> list = sales.Products
+                                      .Where(p => p.CateID == id)
+                                      .ToList();
+            return View(list);
+        }
+        // GET: Product/ Detail / id
+        public ActionResult Detail(int id)
+        {
+            List<Product> pro = sales.Products
+                                      .Where(p => p.ProID == id)
+                                      .ToList();
+            return View(pro);
+        }
     }
 }
